17b52ec8e1b302f4e9337b2f9d9c6c8f32367d49
socialgroups <socialgroups@outlook.com>
2016-04-28 17:13:24 -0300

    add simple testrunner file

---
+++ testrunner.js
@@ -0,0 +1,9 @@
+// Simple Javascript example
+
+console.log('Loading a web page');
+var page = require('webpage').create();
+var url = 'http://phantomjs.org/';
+page.open(url, function (status) {
+    //Page is loaded!
+    phantom.exit();
+});
